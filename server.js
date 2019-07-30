6c5a18994ef726a350b33c62d6eb3445901c2a47
Muosvr <lujason2015@gmail.com>
2019-07-30 19:57:35 -0400

    deployment

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('./client/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/test", (req, res) => res.send("Hello"));
+}
+
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
