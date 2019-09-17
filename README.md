5aa994bf0f967420dbbbdb727d447fa786be0025
Muosvr <lujason2015@gmail.com>
2019-09-17 11:30:05 -0400

    first commit

--- README.md
+++
@@ -1,5 +0,0 @@
-My Portfolio
-
-# Todos
-1. Convert to React Material UI
-2. Serve content data from backend
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import files from './FileLoader';
 
 export default function Home() {
   return (
-    <div id="home" style={{ backgroundColor: "#303233", paddingTop: "35vh", paddingBottom: "10vh" }}>
+    <div id="home" style={{ backgroundColor: "#303233", paddingTop: "10%", paddingBottom: "10vh" }}>
 
       <h1 style={{ fontSize: "5vw", marginLeft: "10vw", color: "white" }} >Hello!</h1>
       <p style={{ marginTop: "120px" }}>
